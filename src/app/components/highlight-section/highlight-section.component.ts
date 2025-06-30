dd8bcb0d4ba0716c00336bf4b028c09f89c200a6
chaimae <ghazichaymae@gmail.com>
2025-06-30 16:00:27 +0100

    news section

--- src/app/components/highlight-section/highlight-section.component.ts
+++ src/app/components/highlight-section/highlight-section.component.ts
@@ -1,11 +1,78 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-highlight-section',
-  imports: [],
   templateUrl: './highlight-section.component.html',
-  styleUrl: './highlight-section.component.css'
+  styleUrls: ['./highlight-section.component.css'],
+  imports: [CommonModule],
 })
 export class HighlightSectionComponent {
+  activeCardIndex: number | null = null;
 
+setActiveCard(index: number) {
+  this.activeCardIndex = index;
+}
+  currentIndex = 0;
+  itemsPerPage = 4;
+  articles = [
+    {
+      title: 'VFS Global Leverages SAP Software to Power Digital Cross-Border Mobility',
+      author: 'SAP NEWS',
+      date: 'May 2, 2025',
+      image: 'assets/article1.jpg'
+    },
+    {
+      title: 'Six AI-Accelerated, Proactive, and Preventative Methods Transforming SAP’s Customer Support',
+      author: 'Stefan Sterne',
+      date: 'May 8, 2025',
+      image: 'assets/article2.jpg'
+    },
+    {
+      title: 'SAP Named a Leader in 2025 Gartner® Magic Quadrant™ for Warehouse Management Systems',
+      author: 'Till Dengel',
+      date: 'May 9, 2025',
+      image: 'assets/article3.jpg'
+    },{
+      title: 'VFS Global Leverages SAP Software to Power Digital Cross-Border Mobility',
+      author: 'SAP NEWS',
+      date: 'May 2, 2025',
+      image: 'assets/article1.jpg'
+    },
+    {
+      title: 'Six AI-Accelerated, Proactive, and Preventative Methods Transforming SAP’s Customer Support',
+      author: 'Stefan Sterne',
+      date: 'May 8, 2025',
+      image: 'assets/article2.jpg'
+    },
+    {
+      title: 'SAP Named a Leader in 2025 Gartner® Magic Quadrant™ for Warehouse Management Systems',
+      author: 'Till Dengel',
+      date: 'May 9, 2025',
+      image: 'assets/article3.jpg'
+    }
+  ];
+
+  // Aller à l'article précédent
+   get visibleArticles() {
+    return this.articles.slice(this.currentIndex, this.currentIndex + this.itemsPerPage);
+  }
+
+  next() {
+    if (this.currentIndex + this.itemsPerPage < this.articles.length) {
+      this.currentIndex += this.itemsPerPage;
+    }
+  }
+
+  prev() {
+    if (this.currentIndex - this.itemsPerPage >= 0) {
+      this.currentIndex -= this.itemsPerPage;
+    }
+  }
+
+  // Aller directement à un index donné
+  goTo(index: number) {
+    this.currentIndex = index;
+  }
 }

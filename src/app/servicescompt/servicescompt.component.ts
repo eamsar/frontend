58992da55cec4be81b3ca02e017db26a7064ec0e
chaimae <ghazichaymae@gmail.com>
2025-07-03 19:57:16 +0100

    servicescomp

---
+++ src/app/servicescompt/servicescompt.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servicescompt',
+  imports: [],
+  templateUrl: './servicescompt.component.html',
+  styleUrl: './servicescompt.component.css'
+})
+export class ServicescomptComponent {
+
+}

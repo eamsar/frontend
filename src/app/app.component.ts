3ff9ff7e6e9152506732511fda6510d4fa0658aa
chaimae <ghazichaymae@gmail.com>
2025-07-07 12:32:42 +0100

    serv background color changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
   title = 'web-frontend';

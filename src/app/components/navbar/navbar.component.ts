e5f3ea6a7d9ffcf16cc66ff1823036d8a15e28bd
chaimae <ghazichaymae@gmail.com>
2025-07-09 22:28:54 +0100

    menu changes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
-  imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
+  standalone: true,
+  imports: [CommonModule],
 })
 export class NavbarComponent {
+isScrolled = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 0;
+  }
 }

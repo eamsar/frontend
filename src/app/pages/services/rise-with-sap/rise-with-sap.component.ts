e5d25cdb88c9551b88cd0e6a176ccc3ac1e241e4
Sanae Aziz <sanae.aziz@atliontech.com>
2025-07-10 13:51:17 +0100

    create services page

--- src/app/pages/services/rise-with-sap/rise-with-sap.component.ts
+++ src/app/pages/services/rise-with-sap/rise-with-sap.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "../../../components/navbar/navbar.component";
-
+import { NavbarComponent } from '../../../components/navbar/navbar.component';
+import { FooterComponent } from '../../../components/footer/footer.component';
+import { ContactFormComponent } from '../../../components/contact-form/contact-form.component';
 @Component({
   standalone:true,
   selector: 'app-rise-with-sap',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,FooterComponent,ContactFormComponent],
   templateUrl: './rise-with-sap.component.html',
   styleUrl: './rise-with-sap.component.css'
 })

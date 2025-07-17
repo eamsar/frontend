fa0645729b9cbe189a1ff704eb2be6408b196285
eamsar <106921247+eamsar@users.noreply.github.com>
2025-07-17 13:48:15 +0100

    Commit

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,22 +1,37 @@
 import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
 })
 export class NavbarComponent {
 isScrolled = false;
-
+searchTerm = "" ;
+constructor(private router: Router) {}
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.scrollY > 0;
   }
     showSearch = false;
 
-  toggleSearch() {
-    this.showSearch = !this.showSearch;
+ toggleSearch() {
+  this.showSearch = !this.showSearch;
+}
+
+closeSearch() {
+  this.showSearch = false;
+}
+
+
+  onSearch() {
+      console.log('Searching for:', this.searchTerm);
+  if (this.searchTerm.trim()) {
+    this.router.navigate(['/search'], { queryParams: { q: this.searchTerm.trim() } });
   }
+}
 }
